f46e7692a991832c86416d10e07f033092e2ccc1
Rustam GUSEYNOV <rustam.guseynov@exakis-nelite.com>
2020-10-26 15:23:45 +0100

    add connection to sql server

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -1,11 +1,19 @@
+import os
 import datetime
 import logging
 
+import requests
+import pyodbc
+
 import azure.functions as func
+
+# from azure.identity import DefaultAzureCredential
+# from azure.identity import ManagedIdentityCredential
+# from azure.keyvault.secrets import SecretClient
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    print("enter func")
+    logging.info('enter function')
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
@@ -13,3 +21,27 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    password = os.environ['SQL_SERVER_CONNECTION']
+
+    # credential = DefaultAzureCredential()
+    # credential = ManagedIdentityCredential()
+
+    # secret_client = SecretClient(vault_url="https://tradingkeyvault.vault.azure.net/", credential=credential)
+    # secret = secret_client.get_secret("sqlserver-trading")
+
+    # logging.info(f"secret name is: {secret.name}")
+
+    cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:sqlserver-trading.database.windows.net,1433;Database=financial;Uid=MasterTrader;Pwd="+password+";Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    cursor = cnxn.cursor()
+
+    logging.info("connection done")
+
+    cursor.execute("SELECT * FROM {}".format('SP500')) 
+    # row = cursor.fetchone()
+    rows = cursor.fetchall()
+
+    logging.info("row fetched")
+
+    logging.info(rows)
+
